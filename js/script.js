2241da6b5f5086f84e028b0980ffa6b8cea8408f
Ramazan <r.tokoshev@gmail.com>
2021-10-06 02:38:19 +0600

    added regular expression to my slide and dots

--- js/script.js
+++ js/script.js
@@ -391,11 +391,15 @@ window.addEventListener('DOMContentLoaded', function () {
 		dots.push(dot);
 	}
 
+	function deleteNotDigits(srt) {
+		return + srt.replace(/\D/g, '');
+	}
+
 	next.addEventListener('click', () => {
-		if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+		if (offset == deleteNotDigits(width) * (slides.length - 1)) {
 			offset = 0;
 		} else {
-			offset += +width.slice(0, width.length - 2);
+			offset += deleteNotDigits(width);
 		}
 
 		slidesField.style.transform = `translateX(-${offset}px)`;
@@ -418,9 +422,9 @@ window.addEventListener('DOMContentLoaded', function () {
 
 	prev.addEventListener('click', () => {
 		if (offset == 0) {
-			offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+			offset =  deleteNotDigits(width) * (slides.length - 1);
 		} else {
-			offset -= +width.slice(0, width.length - 2);
+			offset -= deleteNotDigits(width);
 		}
 
 		slidesField.style.transform = `translateX(-${offset}px)`;
@@ -446,7 +450,7 @@ window.addEventListener('DOMContentLoaded', function () {
 			const slideTo = e.target.getAttribute('data-slide-to');
 
 			slideIndex = slideTo;
-			offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+			offset = deleteNotDigits(width) * (slideTo - 1);
 
 			slidesField.style.transform = `translateX(-${offset}px)`;
 
